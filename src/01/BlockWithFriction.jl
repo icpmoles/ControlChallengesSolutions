2ca44aca3e6e809a542f1ed35272c738e89a44ab
Iacopo Moles <iacopo@icpmol.es>
2025-06-03 12:09:05 +0200

    full state feedback

--- src/01/BlockWithFriction.jl
+++ src/01/BlockWithFriction.jl
@@ -1,5 +1,6 @@
 using ControlSystems
 using Plots
+using LinearAlgebra
 # System parameters
 g = 9.81
 α = 0.0 # slope
@@ -8,7 +9,7 @@ x_0 = -2.0 # starting position
 dx_0 = 0.0 # starting velocity
 τ = 20.0 # torque constant 
 
-
+# State Space Matrix
 A = [ 0  1 0 
     0 -μ 1
     0  0  -τ
@@ -27,18 +28,27 @@ sys = ss(A, B, C, 0)      # Continuous
 bodeplot(tf(sys))
 current_poles = poles(sys)
 
-p = -[10.0 20.0 40.0];
-C_full = Matrix{Float64}(I, 3, 3)
+p = -[10 15 12];
+observability(A, C)
+controllability(A, B)
 
-# L = place(sys, 2*p, :o)
-K = place(sys, p, :c)
 
-controller = ss([0 0; 0 0],[0 0; 0 0],[0 0],K[1:2]')
+L = place(sys, p, :c)
 
-closedLoop = feedback(sys, controller)
+
+# L = place(A', C', 5*p, opt=:c) # error, let's use kalman
+R1 = diagm([0.01, 0.02, 0.03])
+R2 = diagm([0.005, 0.002])
+K = kalman(sys, R1, R2; direct = false)
+
+cont = observer_controller(sys, L, K; direct=false)
+
+
+closedLoop = feedback(sys,cont)
 
 clpoles = poles(closedLoop)
+setPlotScale("dB")
 
-gangoffourplot(sys, controller; minimal=true)
+gangoffourplot(sys, cont; minimal=true)
 
-bodeplot(closedLoop)
+bodeplot(closedLoop[1,1],0.1:40)
