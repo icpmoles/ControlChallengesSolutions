8198e069b9f7e0e7a22b2b5db09cdcfa4c518adc
Iacopo Moles <iacopo@icpmol.es>
2025-07-12 17:13:37 +0200

    control filter finally works

---
+++ docs/_extensions/shortcontrol/shortcontrol.lua
@@ -0,0 +1,20 @@
+
+
+function CodeBlock(code)
+  if (code.classes:includes("control")) then
+   
+    targetType = pandoc.utils.stringify(code.attributes["target"])
+    local codePayload = pandoc.utils.stringify(code.text)
+    local IfrrameUrl = "https://pages.icpmol.es/ControlChallenges/index.html"
+    local url = IfrrameUrl .. "?target=" .. targetType .. '&code=' .. quarto.base64.encode(codePayload)
+    local cssStyle = 'width:100%;height:90vh;border-width:3px;border-style:dashed'
+
+
+
+    if quarto.doc.isFormat('html')  then
+      return pandoc.RawBlock('html',  '<iframe src="' .. url .. '" style=' .. cssStyle .. '></iframe>' .. '<a href='.. url ..' class="simulation-url">Open Simulation</a>')
+    else
+      return pandoc.Link({pandoc.Str "Open Simulation"}, url)
+    end
+  end
+end
